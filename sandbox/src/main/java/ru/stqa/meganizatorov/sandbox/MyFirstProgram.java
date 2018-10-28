0a1407461b1875ffed7903f62bffe092f13b0376
meganizatorov <lovecolormusic90@gmail.com>
2018-10-28 16:38:30 +0500

    3

--- sandbox/src/main/java/ru/stqa/meganizatorov/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/meganizatorov/sandbox/MyFirstProgram.java
@@ -2,9 +2,8 @@ package ru.stqa.meganizatorov.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
-		System.out.println ("Hello, world!");
-	}
-  
 }
