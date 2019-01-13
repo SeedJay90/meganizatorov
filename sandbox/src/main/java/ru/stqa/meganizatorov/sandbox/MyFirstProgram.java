25259d3fee9a2bc5f805819bd92669e787b576ed
meganizatorov <lovecolormusic90@gmail.com>
2019-01-13 15:05:34 +0500

    1

--- sandbox/src/main/java/ru/stqa/meganizatorov/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/meganizatorov/sandbox/MyFirstProgram.java
@@ -3,13 +3,7 @@ package ru.stqa.meganizatorov.sandbox;
 public class MyFirstProgram { //обьявление класса
 
   public static void main(String[] args) {
-    String somebody = "world";
-    System.out.println("Hello, " + somebody + "!");
-
-    double l = 8.2;
-    double s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
-
+    System.out.println("Hello, world!");
   }
 
 }
